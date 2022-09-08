a155da044620d94f13d8491c54eb7d2dec1bac0e
clarkjoao <46800211+clarkjoao@users.noreply.github.com>
2022-09-08 16:56:40 +0100

    feat: create https server (#13)
    * feat: create https server
    
    * fix: server callback
    
    * chore: imports
    
    * add: note at .env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { Express } from "express";
+import { readFileSync } from "fs";
+import { createServer } from "https";
 import { router } from "src/routes";
 dotenv.config();
 
@@ -8,7 +10,7 @@ const app: Express = express();
 const port = process.env.WEB_EVENTS_PORT || 3334;
 
 var corsOptions = {
-  origin: process.env.WEBAPP_URL || "http://localhost:3333",
+  origin: process.env.WEBAPP_URL || "http://localhost:3334",
   optionsSuccessStatus: 200,
 };
 
@@ -17,6 +19,26 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
-});
+if (process.env.SSL_ENABLE === "true") {
+  const keyPath = process.env.SSL_PRIVATE_KEY_PATH;
+  const certPath = process.env.SSL_CERT_PATH;
+
+  if (!keyPath || !certPath) {
+    throw Error("Missing SSLKeyPath or SSLCertPath");
+  }
+
+  createServer(
+    {
+      key: readFileSync(keyPath, "utf8"),
+      cert: readFileSync(certPath, "utf8"),
+    },
+    app
+  ).listen(port, () => {
+    console.log("Running a secure https server...");
+    console.log(`Server is running at HTTPS:${port}`);
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`Server is running at HTTP:${port}`);
+  });
+}
