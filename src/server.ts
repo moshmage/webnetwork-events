3b1239c28f46050b565f59c4c8b36d69ac298ee2
MoshMage <moshmage@gmail.com>
2022-09-16 10:30:01 +0100

    add a logger for corsOptions on server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,13 @@ import express, { Express } from "express";
 import { readFileSync } from "fs";
 import { createServer } from "https";
 import { router } from "src/routes";
+import loggerHandler from "./utils/logger-handler";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.WEB_EVENTS_PORT || 3334;
 
-var corsOptions: CorsOptions = {
+const corsOptions: CorsOptions = {
   origin: process.env.WEBAPP_URL || "http://localhost:3334",
   optionsSuccessStatus: 200,
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
@@ -20,6 +21,11 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(router);
 
+const listen = () => {
+  loggerHandler.warn(`API Listening on ${port} over HTTP${process.env.SSL_ENABLED ? "S" : ""}`);
+  loggerHandler.info(`API corsOptions`, corsOptions);
+}
+
 if (process.env.SSL_ENABLED === "true") {
   const keyPath = process.env.SSL_PRIVATE_KEY_PATH;
   const certPath = process.env.SSL_CERT_PATH;
@@ -28,18 +34,8 @@ if (process.env.SSL_ENABLED === "true") {
     throw Error("Missing SSLKeyPath or SSLCertPath");
   }
 
-  createServer(
-    {
-      key: readFileSync(keyPath, "utf8"),
-      cert: readFileSync(certPath, "utf8"),
-    },
-    app
-  ).listen(port, () => {
-    console.log("Running a secure https server...");
-    console.log(`Server is running at HTTPS:${port}`);
-  });
+  createServer({key: readFileSync(keyPath, "utf8"), cert: readFileSync(certPath, "utf8"),}, app)
+    .listen(port, listen);
 } else {
-  app.listen(port, () => {
-    console.log(`Server is running at HTTP:${port}`);
-  });
+  app.listen(port, listen);
 }
