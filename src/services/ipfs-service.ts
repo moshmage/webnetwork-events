29138b56d0f2ac11f553d528fe8fa2f2e60520f5
clarkjoao <46800211+clarkjoao@users.noreply.github.com>
2022-10-27 12:32:05 +0100

    DEV-951 install chromium on docker enviroment (#46)
    * DEV-951 install chromium on docker enviroment
    
    * math ipfs env to front
    
    * math ipfs env to front
    
    * change ipfs upload base url
    
    * DEV-951 restore unique envs
    
    * DEV-951 update seo event

--- src/services/ipfs-service.ts
+++ src/services/ipfs-service.ts
@@ -8,7 +8,7 @@ import loggerHandler from "../utils/logger-handler";
 const {
   IPFS_PROJECT_ID: id,
   IPFS_PROJECT_SECRET: secret,
-  IPFS_BASE: baseURL,
+  IPFS_UPLOAD_ENDPOINT: baseURL,
 } = process.env;
 
 const auth = "Basic " + Buffer.from(id + ":" + secret).toString("base64");
