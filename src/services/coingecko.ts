9ab6dfb3fe4827316ce8bc6ed7a4060dd94cc3a4
Marcus Vinícius <marcusvinici25@gmail.com>
2023-02-14 09:43:25 -0300

    Hotfix/adding try catch coingecko service (#91)
    * Dev 1233 show token already saved on the database as options (#74)
    
    * updating models
    
    * fix
    
    * handle isAllowedColumn
    
    * Bepro 1323 change table of tokens to have column is reward so we dont have dupes on database (#76)
    
    * adding isReward to token model
    
    * adding isReward to change-allowed-tokens-events
    
    * adjusting isReward model
    
    * adjusting validateToken function
    
    * adding logic isTransactional
    
    * adding new columns to network_tokens model
    
    * adjusting removed tokens/network_tokens logic
    
    * adding else-if
    
    * adding try catch
    
    * refactoring to receive log on coingecko
    
    * adding class Logger
    
    * adding or to type Logger.error
    
    * removing or logic
    
    * adjusting logger to Logger.error
    
    * adjusting type looger
    
    ---------
    
    Co-authored-by: Vitor Hugo <vhcsilvaa@gmail.com>

--- src/services/coingecko.ts
+++ src/services/coingecko.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Logger } from "src/utils/logger-handler";
 
 const {
     NEXT_PUBLIC_CURRENCY_MAIN: currency,
@@ -8,23 +9,34 @@ const {
 const COINGECKO_API = axios.create({baseURL: "https://api.coingecko.com/api/v3"});
 
 async function getCoinPrice(search: string, fiat = currency) {
-    if (!enableCoinGecko)
+    Logger.changeActionName("coingecko")
+    if (!enableCoinGecko){
+      Logger.warn("enableCoinGecko env is disabled")
       return 0;
+    }
   
     const coins = await COINGECKO_API.get(`/coins/list?include_platform=false`).then(value => value.data);
+
+    if(!Array.isArray(coins))
+        Logger.warn(coins, "Error to get list coingecko")
+
     const symbols = search.toLowerCase().split(',')
     const coinsData = coins.filter(({symbol}) => symbols.includes(symbol))
 
-    if (coinsData.length < 1)
+    if (coinsData.length < 1){
+      Logger.warn(coinsData, "Error to filter symbol coingecko")
       return 0;
-  
+    }
+      
     const ids = coinsData.map(({id}) => id).join()
 
     const price = await COINGECKO_API.get(`/simple/price?ids=${ids}&vs_currencies=${fiat || 'eur'}`);
 
-    if (!price?.data)
+    if (!price?.data){
+      Logger.log(price.statusText, "Error to get prices coingecko")
       return 0;
-
+    }
+      
     const result: any = {}
 
     coinsData.map(({symbol, id}) => result[symbol] = {[fiat || 'eur']: price?.data?.[id]?.[fiat || 'eur']})
