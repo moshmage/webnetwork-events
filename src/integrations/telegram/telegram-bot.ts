2e142f0a7bbd9f54bb57918093fbc1d05b6e427a
MoshMage <moshmage@gmail.com>
2023-02-14 23:57:56 +0000

    corrects tokens on default exporter

--- src/integrations/telegram/telegram-bot.ts
+++ src/integrations/telegram/telegram-bot.ts
@@ -2,10 +2,10 @@ import {Telegraf} from "telegraf";
 import {Logger} from "../../utils/logger-handler";
 
 export default (() => {
-  if (!process.env.TELEGRAM_CHANNELS) {
-    Logger.debug(`TelegramBot`,`trying to use but missing process.env.TELEGRAM_CHANNELS`)
+  if (!process.env.TELEGRAM_TOKEN) {
+    Logger.debug(`TelegramBot`,`trying to use but missing process.env.TELEGRAM_TOKEN`)
     return null;
   }
 
-  return new Telegraf(process.env.TELEGRAM_TOKEN!)
+  return new Telegraf(process.env.TELEGRAM_TOKEN)
 })();
