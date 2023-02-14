39403da2fce90bff69db23046db68b6bc13cef27
MoshMage <moshmage@gmail.com>
2023-02-14 23:56:56 +0000

    remove debug and add name to logger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,7 @@
-import { Router } from "express";
+import {Router} from "express";
 
-import { eventsRouter } from "./events.routes";
-import { seoRoutes } from "./seo.routes";
-import {sendMessageEnvChannels} from "../integrations/telegram";
-import {NEW_BOUNTY_OPEN} from "../integrations/telegram/messages";
+import {eventsRouter} from "./events.routes";
+import {seoRoutes} from "./seo.routes";
 
 const router = Router();
 
@@ -12,7 +10,6 @@ router.use("/seo", seoRoutes);
 router.use("/past-events", eventsRouter);
 
 router.use("/", async (req, res) => {
-  // sendMessageEnvChannels(`${NEW_BOUNTY_OPEN('19BEPRO', '/bepro/bounty?id=77&repoId=2')}`);
   return res.status(200).json("::");
 });
 
