880427c701b949634cc14893c728bc14dc9ab702
MoshMage <moshmage@gmail.com>
2023-02-16 20:02:50 +0000

    fix logger calls on coingecko.ts

--- src/utils/logger-handler.ts
+++ src/utils/logger-handler.ts
@@ -8,25 +8,4 @@ export const warn = (message: string, ...rest) => Scribal.w(message, ...rest);
 export const debug = (message: string, ...rest) => Scribal.d(message, ...rest);
 export const trace = (message: string, ...rest) => Scribal.d(message, ...rest);
 
-export class Logger {
-  constructor(readonly name: string) {
-  }
-
-  info(message, ...contents) {
-    Scribal.i(message, ...contents, {name: this.name});
-  }
-
-  warn(message, ...contents) {
-    Scribal.w(message, ...contents, {name: this.name});
-  }
-
-  error(message, ...contents) {
-    Scribal.e(message, ...contents, {name: this.name});
-  }
-
-  debug(message, ...contents) {
-    Scribal.d(message, ...contents, {name: this.name});
-  }
-}
-
 export default {error, info, warn, log, debug, trace}
