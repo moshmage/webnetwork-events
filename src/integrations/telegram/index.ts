4dfd352992c006a0d35fb676713d0612135f7103
MoshMage <moshmage@gmail.com>
2023-02-25 11:27:13 +0000

    add catch clause to telegram send message

--- src/integrations/telegram/index.ts
+++ src/integrations/telegram/index.ts
@@ -2,20 +2,24 @@ import TelegramBot from "./telegram-bot";
 import Logger from "../../utils/logger-handler";
 
 export function sendMessageToTelegramChannels(message: string, moreChannels: string[] = []) {
-  if (!process.env.TELEGRAM_CHANNELS) {
-    Logger.debug(`TelegramBot`, `trying to use but missing process.env.TELEGRAM_CHANNELS`)
-    return;
-  }
+  try {
+    if (!process.env.TELEGRAM_CHANNELS) {
+      Logger.debug(`TelegramBot`, `trying to use but missing process.env.TELEGRAM_CHANNELS`)
+      return;
+    }
 
-  for (const channel of JSON.parse(process.env.TELEGRAM_CHANNELS).concat(moreChannels)) {
-    Logger.debug(`TelegramBot`, `Sending message to ${channel}`, message);
-    TelegramBot?.telegram
-      .sendMessage(channel, message)
-      .then(m => {
-        Logger.info(`TelegramBot`, `Sent message to ${channel}`, m.message_id, m.text);
-      })
-      .catch(e => {
-        Logger.error(e, `TelegramBot`, `Failed to send message to ${channel}`, message);
-      });
+    for (const channel of JSON.parse(process.env.TELEGRAM_CHANNELS).concat(moreChannels)) {
+      Logger.debug(`TelegramBot`, `Sending message to ${channel}`, message);
+      TelegramBot?.telegram
+        .sendMessage(channel, message)
+        .then(m => {
+          Logger.info(`TelegramBot`, `Sent message to ${channel}`, m.message_id, m.text);
+        })
+        .catch(e => {
+          Logger.error(`TelegramBot`, `Failed to send message to ${channel}`, message, e.stack);
+        });
+    }
+  } catch (e: any) {
+    Logger.error(`TelegramBot`, e?.message, e.stack)
   }
 }
