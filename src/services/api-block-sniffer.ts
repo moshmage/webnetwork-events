037f543dea8bc833f6a0c42acdac9cfdf41eedd5
Vitor Hugo <vhcsilvaa@gmail.com>
2023-04-21 16:21:29 -0300

    using to block

--- src/services/api-block-sniffer.ts
+++ src/services/api-block-sniffer.ts
@@ -4,8 +4,9 @@ import {BlockSniffer} from "src/services/block-sniffer";
 export class ApiBlockSniffer extends BlockSniffer {
   constructor(readonly web3Host: string,
               readonly mappedEventActions: MappedEventActions,
-              readonly startBlock: number = 0) {
-    super(web3Host, mappedEventActions, startBlock, startBlock + 1, null, 0);
+              readonly startBlock: number = 0,
+              readonly targetBlock: number = 0) {
+    super(web3Host, mappedEventActions, startBlock, targetBlock || startBlock + 1, null, 0);
   }
 
   // remove these because they are not needed on this ephemeral state
