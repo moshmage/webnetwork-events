e3ff05c8c4a509b82dcace81f05b776cfadc81f3
vhcsilva <vhcsilvaa@gmail.com>
2023-10-23 13:17:34 -0300

    add get close fee method

--- src/services/dao-service.ts
+++ src/services/dao-service.ts
@@ -1,4 +1,4 @@
-import { Network_v2, Web3Connection } from "@taikai/dappkit";
+import { NetworkRegistry, Network_v2, Web3Connection } from "@taikai/dappkit";
 
 const {
   NEXT_PUBLIC_WEB3_CONNECTION: rpcHost,
@@ -10,18 +10,22 @@ interface DAOServiceProps {
   web3Host?: string;
   privateKey?: string;
   networkAddress?: string;
+  registryAddress?: string;
 }
 
 export default class DAO {
   web3Connection: Web3Connection;
   network: Network_v2;
+  registry: NetworkRegistry;
   networkAddress: string;
+  registryAddress: string;
 
-  constructor({ web3Connection, web3Host = rpcHost, privateKey = pKey, networkAddress } : DAOServiceProps = {}) {
+  constructor({ web3Connection, web3Host = rpcHost, privateKey = pKey, networkAddress, registryAddress } : DAOServiceProps = {}) {
     if (!web3Host && !web3Connection)
       throw new Error("Missing web3 provider URL or web3 connection");
 
     if (networkAddress) this.networkAddress = networkAddress;
+    if (registryAddress) this.registryAddress = registryAddress;
 
     this.web3Connection = web3Connection || new Web3Connection({
       web3Host,
@@ -40,11 +44,22 @@ export default class DAO {
         await this.network.loadContract();
       }
 
+      if (this.registryAddress) {
+        this.registry = new NetworkRegistry(this.web3Connection, this.registryAddress);
+        
+        await this.network.loadContract();
+      }
+
       return true;
     } catch (error) {
       console.debug("Error starting: ", error);
     }
 
     return false;
+  }
+
+  async getCloseFee(): Promise<number> {
+    const treasury = await this.network.treasuryInfo();
+    return treasury.closeFee;
   }
 }
