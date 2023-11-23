e827f94c34273f4e3819146c2388c0226cf7e9af
MoshMage <moshmage@gmail.com>
2023-11-23 12:34:59 +0000

    return to wait for all settled so we don't need to collect various times from the same collector and instead send all events to the collector

--- src/services/analytics/push.ts
+++ src/services/analytics/push.ts
@@ -4,6 +4,7 @@ import {AnalyticEvents} from "./types/analytics";
 import {error} from "../../utils/logger-handler";
 import {ErrorMessages} from "../../types/error-messages";
 import {AnalyticEventName} from "./types/events";
+import {Collector} from "./collector";
 
 export class Push {
 
@@ -17,14 +18,27 @@ export class Push {
 
   static async events(payload: AnalyticEvents) {
 
+    const collectedEvents: { [k in AnalyticEventName]?: { collector: Collector, events: AnalyticEvents } } = {};
+
     try {
       for (const event of payload) {
         const collectors = Push.getCollectors(event.name);
         for (const collector of collectors) {
           if (collector?.type)
-            await collector.collect(JSON.parse(JSON.stringify(event)))
+            collectedEvents[collector.type] =
+              {
+                ...(collectedEvents[collector.type] || {}),
+                collector,
+                events: [...collectedEvents[collector.type]?.events, event]
+              }
         }
       }
+
+      await Promise.allSettled(
+        Object.values(collectedEvents)
+          .map(({collector, events}) =>
+            collector.collect(JSON.parse(JSON.stringify(events))))
+      )
     } catch (e) {
       error(ErrorMessages.FailedToCollectLog, e?.toString());
     }
