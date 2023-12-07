7bfbc317780f2bf74091f3192ec986a4eb0be076
MoshMage <moshmage@gmail.com>
2023-12-07 11:49:09 +0000

    properly build bcc because sendgrid doesn't

--- src/integrations/send-grid/email-service/email-service.ts
+++ src/integrations/send-grid/email-service/email-service.ts
@@ -16,21 +16,26 @@ class EmailServiceFactory {
     MailService.setApiKey(apiKey);
   }
 
-  async sendEmail(subject: string, bcc: string | string[], html: string): Promise<ClientResponse> {
+  async sendEmail(subject: string, _bcc: string | string[], html: string): Promise<ClientResponse> {
     return new Promise((p, f) => {
       debug(`Sending email (${subject})`);
 
       const sendCallback = (e: Error, result: [ClientResponse, {}]) => {
         if (e) {
-          warn(`Failed to send email to ${bcc.toString()} (${subject})`);
+          warn(`Failed to send email to ${_bcc.toString()} (${subject})`);
           f(e);
         } else {
-          info(`Sent email to ${bcc.toString()} (${subject})`);
+          info(`Sent email to ${_bcc.toString()} (${subject})`);
           p(result[0]);
         }
       }
-
-      MailService.send({subject, html, bcc, from: NEXT_SENDGRID_MAIL_FROM!}, false, sendCallback)
+      const [to, ...bcc] = Array.isArray(_bcc) ? _bcc : [_bcc];
+      MailService.send({
+        subject,
+        html,
+        to, ...bcc.length ? {bcc} : {},
+        from: NEXT_SENDGRID_MAIL_FROM!
+      }, false, sendCallback)
     });
   }
 }
