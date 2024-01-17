4e8bc5328c212bab4b988bf7ce7f8b2eeb3a165a
MoshMage <moshmage@gmail.com>
2024-01-17 10:02:00 +0000

    missing subject for pr-ready action

--- src/integrations/send-grid/notifications/templates/email-info.ts
+++ src/integrations/send-grid/notifications/templates/email-info.ts
@@ -4,6 +4,7 @@ export const EmailNotificationSubjects: { [k in keyof typeof Templates]: string 
   BOUNTY_CREATED: "A task has been created on %s!",
   FUNDING_REQUEST_CREATED: "New funding request has been created on %s!",
   PULL_REQUEST_OPEN: "A new deliverable has been created!",
+  PULL_REQUEST_READY: "A deliverable has been marked as \"ready for review\"!",
   MERGE_PROPOSAL_OPEN: "A new proposal was created!",
   MERGE_PROPOSAL_READY: "A deliverable is ready to be accepted!",
 }
