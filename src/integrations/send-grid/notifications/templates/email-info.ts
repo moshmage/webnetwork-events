0ea0758d2bf757b1cac2ee57b8b0240fff9ece8e
MoshMage <moshmage@gmail.com>
2024-01-17 10:05:54 +0000

    add email subject for close_bounty action

--- src/integrations/send-grid/notifications/templates/email-info.ts
+++ src/integrations/send-grid/notifications/templates/email-info.ts
@@ -2,6 +2,7 @@ import {Templates} from "./index";
 
 export const EmailNotificationSubjects: { [k in keyof typeof Templates]: string } = {
   BOUNTY_CREATED: "A task has been created on %s!",
+  BOUNTY_CLOSED: "A task has bee closed!",
   FUNDING_REQUEST_CREATED: "New funding request has been created on %s!",
   PULL_REQUEST_OPEN: "A new deliverable has been created!",
   PULL_REQUEST_READY: "A deliverable has been marked as \"ready for review\"!",
--- src/integrations/send-grid/notifications/templates/index.ts
+++ src/integrations/send-grid/notifications/templates/index.ts
@@ -2,6 +2,7 @@ import {AnalyticEventName} from "../../../../services/analytics/types/events";
 
 export const Templates: { [k in AnalyticEventName]?: string } = {
   BOUNTY_CREATED: "base-template.hbs",
+  BOUNTY_CLOSED: "base-template.hbs",
   FUNDING_REQUEST_CREATED: "base-template.hbs",
   PULL_REQUEST_OPEN: "base-template.hbs",
   PULL_REQUEST_READY: "base-template.hbs",
