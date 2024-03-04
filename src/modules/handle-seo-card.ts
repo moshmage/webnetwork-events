6ef1783bb728dac5f5b8c811df83f9901e0a8e2d
vhcsilva <vhcsilvaa@gmail.com>
2024-03-04 15:57:26 -0300

    fix saving null hash

--- src/modules/handle-seo-card.ts
+++ src/modules/handle-seo-card.ts
@@ -35,9 +35,8 @@ export default async function updateSeoCardBounty(bountyId: number, action: stri
       const { hash } = await ipfsService.add(card);
       if (!hash) {
         logger.warn(`${action} Failed to get hash from IPFS for ${dbBounty.id}`);
-      }
-
-      await dbBounty.update({ seoImage: hash });
+      } else
+        await dbBounty.update({ seoImage: hash });
 
       logger.debug(`${action} - updateSeoCardBounty: Updated - bountyId: ${dbBounty.id}`);
     } else logger.debug(`${action} - updateSeoCardBounty: Error bounty not found `);
