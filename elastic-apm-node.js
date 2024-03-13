fa2049374aa75a8b4e9d53e241145f3fbfa0aea8
MoshMage <moshmage@gmail.com>
2024-03-13 14:02:33 +0000

    more NEXT_ changes

--- elastic-apm-node.js
+++ elastic-apm-node.js
@@ -1,9 +1,9 @@
 require(`dotenv`).config();
 
 module.exports = {
-  serverUrl: process.env.ELASTIC_APM_SERVER_URL,  // E.g. https://my-deployment-name.apm.us-west2.gcp.elastic-cloud.com
-  secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
-  serviceName: process.env.ELASTIC_APM_SERVICE_NAME,
-  active: process.env.ELASTIC_APM_ACTIVE === "true",
-  logLevel: process.env.ELASTIC_APM_LOG_LEVEL
+  serverUrl: process.env.NEXT_ELASTIC_APM_SERVER_URL,  // E.g. https://my-deployment-name.apm.us-west2.gcp.elastic-cloud.com
+  secretToken: process.env.NEXT_ELASTIC_APM_SECRET_TOKEN,
+  serviceName: process.env.NEXT_ELASTIC_APM_SERVICE_NAME,
+  active: process.env.NEXT_ELASTIC_APM_ACTIVE === "true",
+  logLevel: process.env.NEXT_ELASTIC_APM_LOG_LEVEL
 }
