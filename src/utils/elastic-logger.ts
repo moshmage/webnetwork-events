2e0de63736211de75da7c4242dae0310fae522de
MoshMage <moshmage@gmail.com>
2024-03-13 12:44:40 +0000

    match LOG_APP_NAME with ux counterpart so we don't need two

--- src/utils/elastic-logger.ts
+++ src/utils/elastic-logger.ts
@@ -5,7 +5,7 @@ const {
   NEXT_ELASTIC_SEARCH_URL: node,
   NEXT_ELASTIC_SEARCH_USERNAME: username,
   NEXT_ELASTIC_SEARCH_PASSWORD: password,
-  LOG_APP_NAME: index = "webnetwork-events-logs"
+  NEXT_LOG_APP_NAME: index = "webnetwork-events-logs"
 } = process.env;
 
 export const elasticLoggerMaker = (): LoggerPlugin => ({
