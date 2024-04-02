6b159c2bd7bca33a0716eed5486feaffabea47ba
MoshMage <moshmage@gmail.com>
2024-04-02 09:50:36 +0100

    add try-catch to the job processor to be on the safe side

--- src/chain-reader/index.ts
+++ src/chain-reader/index.ts
@@ -2,18 +2,28 @@ import process from "node:process";
 import {Worker} from "bullmq";
 import {NETWORK_EVENTS, REGISTRY_EVENTS} from "../modules/chain-events";
 import {getWeb3Host} from "../utils/get-web3-host";
+import loggerHandler from "../utils/logger-handler";
 
 const {NEXT_REDIS_HOST: host, NEXT_REDIS_PORT: port, NEXT_ENV_NAME: envName,} = process.env;
 
 const connection = {connection: {host, port: port && +port || 6379}}
 
+
 const networkProcessor = async ({data}) => {
-  /* Network actions need `web3host` information, so they can fetch information from the chain */
-  await NETWORK_EVENTS[data.event]?.({...data, connection: await getWeb3Host(data.chainId)});
+  try {
+    /* Network actions need `web3host` information, so they can fetch information from the chain */
+    await NETWORK_EVENTS[data.event]?.({...data, connection: await getWeb3Host(data.chainId)});
+  } catch (e: any) {
+    loggerHandler.error(`Error NETWORK parsing ${data.event}: ${e.message}`);
+  }
 }
 
 const registryProcessor = async ({data}) => {
-  await REGISTRY_EVENTS[data.event]?.(data);
+  try {
+    await REGISTRY_EVENTS[data.event]?.(data);
+  } catch (e: any) {
+    loggerHandler.error(`Error parsing REGISTRY event ${data.event}: ${e.message}`);
+  }
 }
 
 new Worker(`${envName}_network_queue`, networkProcessor, connection);
